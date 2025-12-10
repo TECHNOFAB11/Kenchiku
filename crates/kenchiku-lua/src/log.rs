eadcc5be82984650204f708d2132a7f90b5483c0
technofab <admin@technofab.de>
2025-12-10 14:34:40 +0100

    chore(lua): handle lua print function ourselves for later mcp integration

--- crates/kenchiku-lua/src/log.rs
+++ crates/kenchiku-lua/src/log.rs
@@ -7,9 +7,18 @@ impl LuaLog {
     pub fn register(lua: &Lua, _context: Context) -> Result<()> {
         let warn_fun = lua.create_function(|_, message: String| {
             tracing::warn!("Warning from scaffold: {}", message);
+            // TODO: also write to writer of context, see below
             Ok(())
         })?;
         lua.globals().set("warn", warn_fun)?;
+
+        let print_fun = lua.create_function(|_, message: String| {
+            // TODO: add Writer to context which specifies where to write these messages to (for
+            // mcp)
+            println!("{}", message);
+            Ok(())
+        })?;
+        lua.globals().set("print", print_fun)?;
 
         Ok(())
     }
